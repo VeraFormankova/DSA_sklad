cbeec0d154698c35f03347e8f8c1659d54406201
zf <zdenek.frydryn@gmail.com>
2024-12-06 21:59:08 +0100

    Pridane komentare

--- sklad_dsa.cpp
+++ sklad_dsa.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// Struktura pro ukladani informaci o zbozi
 class Record {
 public:
     int id;
@@ -28,6 +29,11 @@ class Sklad {
 private:
     Record* root;
 
+    // Vlozi novou polozku na sklad
+    // Pokud je node nullptr, vytvori novy uzel
+    // Pokud je id mensi nez id nodu, vlozi se do leveho podstromu
+    // Pokud je id vetsi nez id nodu, vlozi se do praveho podstromu
+    // Pokud je id rovno id nodu, aktualizuje se pocet kusu a datum posledniho naskladneni
     Record* vloz(Record* node, int id, string title, unsigned int ks, double price, string category) {
         if (node == nullptr) {
             return new Record(id, title, ks, price, category);
@@ -79,36 +85,51 @@ private:
         }
     }
 
+    // Vyhleda polozku podle ID
+    // Pokud node je nullptr nebo node->id je rovno hledanemu ID, vrati node
     Record* vyhledat_id(Record* node, int id) {
         if (node == nullptr || node->id == id)
             return node;
 
+        // Pokud je id mensi nez id nodu, hleda v levem podstromu
         if (id < node->id)
             return vyhledat_id(node->left, id);
-
+        // Pokud je id vetsi nez id nodu, hleda v pravem podstromu
         return vyhledat_id(node->right, id);
     }
 
+    // Odstrani polozku podle ID
     Record* odstranit(Record* node, int id) {
+        // Pokud je node nullptr, vrati nullptr, to se stane pokud je uzel prazdny
         if (node == nullptr) return nullptr;
 
+        // hledani polozky v levem podstromu. Pokud je id mensi nez id nodu, hleda v levem podstromu
         if (id < node->id) {
             node->left = odstranit(node->left, id);
         }
+        // hledani polozky v pravem podstromu. Pokud je id vetsi nez id nodu, hleda v pravem podstromu
         else if (id > node->id) {
             node->right = odstranit(node->right, id);
         }
+        // Pokud je id rovno id nodu, odstrani uzel
         else {
+            // pokud levy nema potomka, vytvori se docasny uzel a priradi se pravy potomek
+            // nalezeny se smaze
             if (node->left == nullptr) {
                 Record* temp = node->right;
                 delete node;
                 return temp;
             }
+            // pokud pravy nema potomka, vytvori se docasny uzel a priradi se levy potomek
+            // nalezeny se smaze
             else if (node->right == nullptr) {
                 Record* temp = node->left;
                 delete node;
                 return temp;
             }
+            // pokud uzel ma dva potomky, najde se nejmensi uzel v pravem podstromu
+            // tim se zajisti, ze se zachova struktura binarniho stromu
+            // vsechno v levem podstromu je mensi nez nejnižší uzel v pravem podstromu
             else {
                 Record* temp = min_value_node(node->right);
                 node->id = temp->id;
@@ -123,6 +144,7 @@ private:
         return node;
     }
 
+    // pomocna funkce pro nalezeni nejmensiho uzlu
     Record* min_value_node(Record* node) {
         Record* current = node;
         while (current && current->left != nullptr)
@@ -130,6 +152,8 @@ private:
         return current;
     }
 
+    // template pro kontrolu vstupu od uzivatele
+    // pouze pro numerické hodnoty
     template <typename T>
     void check_numeric_input(T& input, const string& prompt) {
         while (true) {
@@ -137,6 +161,7 @@ private:
             cin >> input;
 
             if (cin.fail()) {
+                // Vyčištění vstupního bufferu před čtením řetězce
                 cin.clear();
                 cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
                 cout << "Neplatný vstup. Zadejte správný datový typ\n";
@@ -147,6 +172,8 @@ private:
         }
     }
 
+    // template pro kontrolu vstupu od uzivatele
+    // pouze pro řetězce
     void check_string_input(string& input, const string& prompt) {
     while (true) {
         cout << prompt;
@@ -161,14 +188,17 @@ private:
 }
 
 public:
+    // inicializace skladu s prazdnym korenem
     Sklad() : root(nullptr) {}
 
+    // funkce pro vlozeni nove polozky na sklad
     void vloz_na_sklad() {
         int id;
         string title, category;
         unsigned int ks;
         double price;
 
+        // probehne kontrola vstupu od uzivatele
         check_numeric_input(id, "Zadejte ID výrobku: ");
         check_string_input(category, "Zadejte název výrobku: ");
         check_numeric_input(ks, "Zadejte počet na skladu: ");
@@ -179,6 +209,7 @@ public:
         cout << "Položka byla přidána na sklad.\n";
     }
 
+    // funkce pro vypis obsahu skladu
     void vypis() {
         if (root == nullptr) {
             cout << "Sklad je prázdný.\n";
@@ -188,6 +219,7 @@ public:
         }
     }
 
+    // funkce pro vyhledani polozek podle kategorie
     void hledat_podle_kategorie() {
         string category;
         cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
@@ -197,6 +229,7 @@ public:
         vyhledat_kategorii(root, category);
     }
 
+    // funkce pro vyhledani polozek podle ID
     void hledat_podle_id() {
         int id;
         cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
